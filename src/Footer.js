2820393a42db04ce0b5037bbae003fa411003b36
Ilker Kurtulan <ilker.kurtulan@hotmail.com>
2021-02-06 11:21:23 +0300

    First commit for airbnb clone

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+          <p>© 2021 Airbnb clone! - this is done by Ilker Kurtulan!</p>
+          
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  );
+}
+
+export default Footer;
